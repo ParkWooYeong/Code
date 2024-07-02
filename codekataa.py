4100d6abf67c5b3f5b139fac8adcb02af3ebeaa1
park <uyeongi@naver.com>
2024-07-02 22:36:43 +0900

    코드카타

--- codekataa.py
+++ codekataa.py
@@ -1,5 +1,5 @@
 # 문제 설명
-# 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+# 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.444
 
 def solution(my_string):
     answer = my_string.swapcase()
